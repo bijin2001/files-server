8fa3efa12c455d73db2b818eff83dc043650652d
bijin2001 <bijin.bb.213@gmail.com>
2024-06-02 13:29:13 +0530

    server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server')
 const FolderServer = jsonServer.create()
 const middleware = jsonServer.defaults()
-const route = jsonServer.router('todo.json')
+const route = jsonServer.router('files.json')
 const PORT = 3000 || process.env.PORT
 
 FolderServer.use(middleware)
