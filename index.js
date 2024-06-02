6d7233a4dee9e365cf3defba9a25ed95719ebd47
bijin2001 <bijin.bb.213@gmail.com>
2024-06-02 13:18:59 +0530

    files

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const jsonServer  =require('json-server')
+const jsonServer = require('json-server')
 const FolderServer = jsonServer.create()
 const middleware = jsonServer.defaults()
-const route = jsonServer.router('files.json')
+const route = jsonServer.router('todo.json')
 const PORT = 3000 || process.env.PORT
 
 FolderServer.use(middleware)
 FolderServer.use(route)
-FolderServer.use(PORT,()=>{
-    console.log(`Folder-server port : ${PORT} has successfully deployed`);
+FolderServer.listen(PORT,()=>{
+    console.log(`Folder port: ${PORT} successfully deployed`);
 })
